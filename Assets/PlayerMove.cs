b5c26a80f2058e42807e596335ff45fb1c028dae
Trifon Hristov <77389880+Narniez@users.noreply.github.com>
2023-05-11 20:21:27 +0300

    Movement and Semi Split Screen

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float walkSpeed = 10f;
+
+    Vector2 moveInput;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        Vector3 playerVelocity = new Vector3(moveInput.x * walkSpeed, rb.velocity.y, moveInput.y * walkSpeed);
+        rb.velocity = transform.TransformDirection(playerVelocity);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
