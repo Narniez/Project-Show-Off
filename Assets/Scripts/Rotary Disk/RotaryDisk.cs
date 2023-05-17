480e7ff384152c0225b144ff99509d8da9f19c82
Stanislav <taix97@outlook.com>
2023-05-18 01:02:15 +0300

    Optimizing the rotary disk scripts

--- Assets/Scripts/Rotary Disk/RotaryDisk.cs
+++ Assets/Scripts/Rotary Disk/RotaryDisk.cs
@@ -1,16 +1,20 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RotaryDisk : MonoBehaviour
 {
     [HideInInspector] public RotaryDisk corespondingDisk;
-    [HideInInspector] public List<GameObject> disks = new List<GameObject>();
-
     [SerializeField] private int[] correctDegreesObject;
 
+    private List<Transform> disks = new List<Transform>();
+
+    /// <summary>
+    /// finds the coresponding disk (it needs 2 disks) and add the children to a list
+    /// </summary>
     private void Start()
     {
-        //Get the coresponding disk
+        // Get the corresponding disk
         for (int i = 0; i < transform.parent.childCount; i++)
         {
             GameObject childTransform = transform.parent.GetChild(i).gameObject;
@@ -24,35 +28,38 @@ public class RotaryDisk : MonoBehaviour
                 break;
         }
 
-        //Get the disk pieces
+        // Get the disk pieces
         for (int i = 0; i < transform.childCount; i++)
         {
-            disks.Add(transform.GetChild(i).gameObject);
+            disks.Add(transform.GetChild(i));
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CorrectPuzzlePosition();
-    }
-
-    private bool CorrectPuzzlePosition()
+    /// <summary>
+    /// Checks if the disks are in the correct rotation
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NullReferenceException"></exception>
+    public bool CorrectPuzzlePosition()
     {
         if (corespondingDisk == null)
-            throw new System.NullReferenceException("Coresponding Disk is null!!!!! ADD THE OTHER DISK MADARFAKAR");
-
-        if ((int)disks[0].transform.rotation.eulerAngles.z == (int)correctDegreesObject[0] &&
-            (int)disks[1].transform.rotation.eulerAngles.z == (int)correctDegreesObject[1] &&
-            (int)disks[2].transform.rotation.eulerAngles.z == (int)correctDegreesObject[2] &&
-
-            (int)corespondingDisk.disks[0].transform.rotation.eulerAngles.z == (int)corespondingDisk.correctDegreesObject[0] &&
-            (int)corespondingDisk.disks[1].transform.rotation.eulerAngles.z == (int)corespondingDisk.correctDegreesObject[1] &&
-            (int)corespondingDisk.disks[2].transform.rotation.eulerAngles.z == (int)corespondingDisk.correctDegreesObject[2])
         {
-            Debug.Log("Correct position");
-            return true;
+            throw new NullReferenceException("Coresponding Disk is null! Add the other disk.");
         }
-        return false;
+
+        for (int i = 0; i < disks.Count; i++)
+        {
+            if ((int)disks[i].rotation.eulerAngles.z != correctDegreesObject[i])
+            {
+                return false;
+            }
+
+            if ((int)corespondingDisk.disks[i].rotation.eulerAngles.z != corespondingDisk.correctDegreesObject[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
