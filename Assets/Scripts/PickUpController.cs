77fe1b348929fdd25a00709cf90c1c1676a0af0e
Stanislav <taix97@outlook.com>
2023-05-22 11:19:27 +0300

    Decoupling, Added buttons, Optimization

--- Assets/Scripts/PickUpController.cs
+++ Assets/Scripts/PickUpController.cs
@@ -2,44 +2,41 @@ using UnityEngine;
 
 public class PickUpController : MonoBehaviour
 {
-    public Transform holdAreaObj;   // The target position where the cube should be held
+    public Transform holdAreaObj;
+    public GameObject holdObj;
 
-    public GameObject holdObj;      // The currently held cube
-    private Rigidbody pickUpObjRB;  // Rigidbody component of the held cube
-
-    private Camera cam;             // Reference to the camera component
+    private Rigidbody pickUpObjRB;
+    private Camera cam;
+    private float dist;
 
     private void Start()
     {
-        cam = GetComponent<Camera>();   // Get the camera component attached to the same game object
+        cam = GetComponent<Camera>();
+        dist = Vector3.Distance(cam.transform.position, holdAreaObj.position);
     }
 
     private void Update()
     {
-        float dist = Vector3.Distance(cam.transform.position, holdAreaObj.position);
 
-        // Check if left mouse button is clicked
         if (Input.GetMouseButtonDown(0))
         {
             if (holdObj == null)
             {
-                // If not holding any cube, raycast to check if there's a cube in front of the camera
                 RaycastHit hit;
                 if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), out hit, dist))
                 {
                     if (hit.collider.CompareTag("CompanionCube"))
                     {
-                        PickUpItem(hit.collider.gameObject);  // Pick up the cube if it has the "CompanionCube" tag
+                        PickUpItem(hit.collider.gameObject);
                     }
                 }
             }
             else
             {
-                DropItem();  // Drop the currently held cube
+                DropItem();
             }
         }
 
-        // Move the held cube if there is one
         if (holdObj != null)
         {
             MovePickedObj();
@@ -61,16 +58,16 @@ public class PickUpController : MonoBehaviour
 
     void PickUpItem(GameObject pickUpObj)
     {
-        if (pickUpObj.GetComponent<Rigidbody>() == null) return;  // Check if the cube has a Rigidbody component
+        if (pickUpObj.GetComponent<Rigidbody>() == null) return;
 
         pickUpObjRB = pickUpObj.GetComponent<Rigidbody>();
         pickUpObjRB.useGravity = false;
         pickUpObjRB.drag = 5f;
 
-        pickUpObjRB.transform.parent = holdAreaObj;  // Set the hold area as the parent of the cube to move it along with the area
+        pickUpObjRB.transform.parent = holdAreaObj;
 
-        holdObj = pickUpObj;  // Set the held cube
-        //pickUpObjRB.constraints = RigidbodyConstraints.FreezeRotation;  // Freeze rotation of the cube when held
+        holdObj = pickUpObj;
+        //pickUpObjRB.constraints = RigidbodyConstraints.FreezeRotation;
         holdObj.transform.localRotation = Quaternion.Euler(0,0,0);
     }
 
@@ -78,10 +75,10 @@ public class PickUpController : MonoBehaviour
     {
         pickUpObjRB.useGravity = true;
         pickUpObjRB.drag = 1;
-        //pickUpObjRB.constraints = RigidbodyConstraints.None;  // Release constraints on the cube
+        //pickUpObjRB.constraints = RigidbodyConstraints.None;
 
-        pickUpObjRB.transform.parent = null;  // Remove the hold area as the parent of the cube
+        pickUpObjRB.transform.parent = null;
 
-        holdObj = null;  // Clear the held cube
+        holdObj = null;
     }
 }
