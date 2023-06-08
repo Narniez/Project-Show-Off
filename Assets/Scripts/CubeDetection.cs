ed98f119f7258393d3ad1881c18d12036060b11e
Trifon Hristov <77389880+Narniez@users.noreply.github.com>
2023-06-08 15:17:32 +0300

    MatrixDoor setup

--- Assets/Scripts/CubeDetection.cs
+++ Assets/Scripts/CubeDetection.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CubeDetection : MonoBehaviour
 {
     int cubeCount;
-
-
     [SerializeField] GameObject door;
     // Start is called before the first frame update
     void Start()
