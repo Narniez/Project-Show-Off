2f8a9ecdcf41e4fad22cbcbd56f76e6f759f0195
Trifon Hristov <77389880+Narniez@users.noreply.github.com>
2023-06-22 18:46:30 +0300

    Voice Lines Added Level1

--- Assets/Scripts/VoiceLines.cs
+++ Assets/Scripts/VoiceLines.cs
@@ -4,54 +4,85 @@ using UnityEngine;
 
 public class VoiceLines : MonoBehaviour
 {
-    [SerializeField] private List<AudioClip> puzzleSovledLines;
+    [SerializeField] private List<AudioClip> puzzleSolvedLines;
     [SerializeField] private List<AudioClip> levelSolvedLines;
-    [SerializeField] private List<AudioClip> noPuzzleSovedLines;
+    [SerializeField] private List<AudioClip> noPuzzleSolvedLines;
     public AudioSource audioSource;
 
     [SerializeField] private ColorPuzzle colorPuzzle;
-    [SerializeField] private RotaryDiskHolder rotDiskPuzzle;
+    [SerializeField] private RotationPuzzleHolder rotDiskPuzzle;
     [SerializeField] private LightPuzzle lightPuzzle;
 
     [SerializeField] private int notSolvedTimer;
     private float pTimer;
-    [SerializeField] private int onPuzzleLockedTimer;
 
-    void Start()
+    private bool colorPuzzleSolved;
+    private bool rotDiskPuzzleSolved;
+    private bool lightPuzzleSolved;
+
+    private void Start()
     {
         pTimer = notSolvedTimer;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        pTimer -= Time.deltaTime;
-        int seconds = Mathf.FloorToInt(pTimer);
-        Debug.Log(seconds);
+        // Check if puzzles are attached
         if (colorPuzzle == null || rotDiskPuzzle == null || lightPuzzle == null)
         {
-            Debug.Log("One or more puzzles not attached in the VoiceLines");
-            return;
+            Debug.LogError("One or more puzzles not attached in the VoiceLines script.");
         }
 
-        if (colorPuzzle.isSolved && rotDiskPuzzle.isdone && lightPuzzle.isCompleted)
+    }
+
+    private void Update()
+    {
+
+        PlayVoiceLines();
+       
+    }
+
+    private void PlayVoiceLines()
+    {
+        pTimer -= Time.deltaTime;
+        float seconds = Mathf.CeilToInt(pTimer);
+        Debug.Log(seconds);
+
+        if (colorPuzzle.isSolved && !colorPuzzleSolved)
+        {
+            pTimer = notSolvedTimer;
+            colorPuzzleSolved = true;
+            ChooseLine(puzzleSolvedLines);
+           
+        }
+        else if (rotDiskPuzzle.isSolved && !rotDiskPuzzleSolved)
+        {
+            pTimer = notSolvedTimer;
+            rotDiskPuzzleSolved = true;
+            ChooseLine(puzzleSolvedLines);
+        }
+        else if (lightPuzzle.isCompleted && !lightPuzzleSolved)
+        {
+            pTimer = notSolvedTimer;
+            lightPuzzleSolved = true;
+            ChooseLine(puzzleSolvedLines);
+        }
+        else if (colorPuzzleSolved && rotDiskPuzzleSolved && lightPuzzleSolved)
         {
             audioSource.clip = levelSolvedLines[Random.Range(0, levelSolvedLines.Count)];
             audioSource.Play();
         }
-
-        if (colorPuzzle.isSolved || rotDiskPuzzle.isdone || lightPuzzle.isCompleted)
+        else if(seconds <= 0)
         {
+            ChooseLine(noPuzzleSolvedLines);;
             pTimer = notSolvedTimer;
-            audioSource.clip = puzzleSovledLines[Random.Range(0, levelSolvedLines.Count)];
-            audioSource.Play();
-        }
-
-        if(seconds <= 0)
-        {
-            Debug.Log("PlayLine");
-            audioSource.clip = noPuzzleSovedLines[Random.Range(0, levelSolvedLines.Count)];
-            audioSource.Play();
         }
     }
+
+    void ChooseLine(List<AudioClip> audioClips)
+    {
+        int index = Random.Range(0, audioClips.Count);
+        audioSource.clip = audioClips[index];
+        audioSource.Play();
+        audioClips.RemoveAt(index);
+
+    }
+
 }
